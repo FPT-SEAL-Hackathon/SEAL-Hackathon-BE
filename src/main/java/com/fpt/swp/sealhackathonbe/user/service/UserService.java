33d79419122016e82d60223f9cb1d9a450167288
h1dra1710 <tronghieu171026@gmail.com>
2026-06-07 13:37:43 +0700

    tiếp tục xử lý login

--- src/main/java/com/fpt/swp/sealhackathonbe/user/service/UserService.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/user/service/UserService.java
@@ -1,5 +1,13 @@
 package com.fpt.swp.sealhackathonbe.user.service;
 
+import com.fpt.swp.sealhackathonbe.user.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepo repo;
+
 
 }
