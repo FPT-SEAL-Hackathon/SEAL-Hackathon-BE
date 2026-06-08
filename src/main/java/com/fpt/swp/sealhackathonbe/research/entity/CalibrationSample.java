a85dd057596de3cdecf8a7bb926bfc4ca2daeb03
NamAnh293 <nguyenducnamanh4@gmail.com>
2026-06-08 16:03:32 +0700

    Comment để ko bị lỗi

--- src/main/java/com/fpt/swp/sealhackathonbe/research/entity/CalibrationSample.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/research/entity/CalibrationSample.java
@@ -1,49 +1,49 @@
-package com.fpt.swp.sealhackathonbe.research.entity;
-
-import com.fpt.swp.sealhackathonbe.user.entity.User;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
-import org.hibernate.annotations.Nationalized;
-
-import java.time.Instant;
-
-@Getter
-@Setter
-@Entity
-@Table(name = "CalibrationSamples")
-public class CalibrationSample {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "SampleID", nullable = false)
-    private Integer id;
-
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "RoundID", nullable = false)
-    private Round roundID;
-
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "SubmissionID", nullable = false)
-    private Submission submissionID;
-
-    @Nationalized
-    @Lob
-    @Column(name = "ReferenceScoreJSON")
-    private String referenceScoreJSON;
-
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "AddedByID", nullable = false)
-    private User addedByID;
-
-    @NotNull
-    @ColumnDefault("getutcdate()")
-    @Column(name = "AddedAt", nullable = false)
-    private Instant addedAt;
-
-
-}
+//package com.fpt.swp.sealhackathonbe.research.entity;
+//
+//import com.fpt.swp.sealhackathonbe.user.entity.User;
+//import jakarta.persistence.*;
+//import jakarta.validation.constraints.NotNull;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.hibernate.annotations.ColumnDefault;
+//import org.hibernate.annotations.Nationalized;
+//
+//import java.time.Instant;
+//
+//@Getter
+//@Setter
+//@Entity
+//@Table(name = "CalibrationSamples")
+//public class CalibrationSample {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "SampleID", nullable = false)
+//    private Integer id;
+//
+//    @NotNull
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "RoundID", nullable = false)
+//    private Round roundID;
+//
+//    @NotNull
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "SubmissionID", nullable = false)
+//    private Submission submissionID;
+//
+//    @Nationalized
+//    @Lob
+//    @Column(name = "ReferenceScoreJSON")
+//    private String referenceScoreJSON;
+//
+//    @NotNull
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "AddedByID", nullable = false)
+//    private User addedByID;
+//
+//    @NotNull
+//    @ColumnDefault("getutcdate()")
+//    @Column(name = "AddedAt", nullable = false)
+//    private Instant addedAt;
+//
+//
+//}
