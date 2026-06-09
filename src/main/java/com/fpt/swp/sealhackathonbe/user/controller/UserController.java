3e6f242669c62d8e1032de8adf1b94a2583d60f2
h1dra1710 <tronghieu171026@gmail.com>
2026-06-09 11:20:45 +0700

    tạm thời chưa xử lý

--- src/main/java/com/fpt/swp/sealhackathonbe/user/controller/UserController.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/user/controller/UserController.java
@@ -1,37 +1,27 @@
 package com.fpt.swp.sealhackathonbe.user.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.fpt.swp.sealhackathonbe.user.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
-    @GetMapping("/")
-    public String login(){
-        return "index";
-    }
-    @PostMapping("/login")
-    public String home(){
-
-        return "home";
-    }
-    @GetMapping("/register")
-    public String regis(){
-        return "regis-form";
-    }
-//    @PostMapping("/register")
-//    public Users register(@RequestBody Users user) {
-//        return service.register(user);
-//
+    @Autowired
+    private UserService authService;
+    //    @GetMapping("/")
+//    public String login(){
+//        return "index";
 //    }
-//
 //    @PostMapping("/login")
-//    public String login(@RequestBody Users user) {
+//    public String home(){
 //
-//        return service.verify(user);
+//        return "home";
 //    }
-    
+//    @GetMapping("/register")
+//    public String regis(){
+//        return "regis-form";
+//    }
+
+
 }
 
