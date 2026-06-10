ab9794ec8e0626ce6f95af42b22a22e3388bc9f0
NamAnh293 <nguyenducnamanh4@gmail.com>
2026-06-10 13:12:47 +0700

    Tận dụng Orm của Spring Data JPa

--- src/main/java/com/fpt/swp/sealhackathonbe/team/entity/Disqualifications.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/team/entity/Disqualifications.java
@@ -1,5 +1,7 @@
 package com.fpt.swp.sealhackathonbe.team.entity;
 
+import com.fpt.swp.sealhackathonbe.submission.entity.Submissions;
+import com.fpt.swp.sealhackathonbe.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,14 +28,26 @@ public class Disqualifications {
   @Column(name = "TeamID")
   private UUID teamId;
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "TeamID", insertable = false, updatable = false)
+  private Teams team;
+
   @Column(name = "SubmissionID")
   private UUID submissionId;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "SubmissionID", insertable = false, updatable = false)
+  private Submissions submission;
 
   @Column(name = "Reason", nullable = false, columnDefinition = "NVARCHAR(MAX)")
   private String reason;
 
   @Column(name = "DisqualifiedByID", nullable = false)
   private UUID disqualifiedById;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "DisqualifiedByID", nullable = false, insertable = false, updatable = false)
+  private User disqualifiedBy;
 
   @Column(name = "DisqualifiedAt", nullable = false)
   private LocalDateTime disqualifiedAt;
@@ -47,6 +61,10 @@ public class Disqualifications {
 
   @Column(name = "ReversedByID")
   private UUID reversedById;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "ReversedByID", insertable = false, updatable = false)
+  private User reversedBy;
 
   @Column(name = "ReversalReason", columnDefinition = "NVARCHAR(MAX)")
   private String reversalReason;
