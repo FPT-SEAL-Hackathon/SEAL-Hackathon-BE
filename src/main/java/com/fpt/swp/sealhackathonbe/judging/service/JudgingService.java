6c8077f1f0e23e3baa37ddbec2cfb3104a602773
NamAnh293 <nguyenducnamanh4@gmail.com>
2026-06-10 13:34:59 +0700

    xóa market conflict

--- src/main/java/com/fpt/swp/sealhackathonbe/judging/service/JudgingService.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/judging/service/JudgingService.java
@@ -8,14 +8,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface JudgingService {
-<<<<<<< Updated upstream
-    void recordScore(ScoreSubmissionDTO dto);
-    List<JudgingDTO> getScoresBySubmission(UUID submissionId);
-    List<JudgingDTO> getScoresByJudge(UUID judgeUserId);
-    List<AssignmentDTO> getJudgeAssignments(UUID judgeUserId);
-=======
     void recordJudging(ScoreSubmissionDTO dto);
     List<JudgingDTO> getScoresBySubmission(UUID submissionId);
     List<JudgingDTO> getScoresByJudge(UUID judgeUserId);
->>>>>>> Stashed changes
 }
