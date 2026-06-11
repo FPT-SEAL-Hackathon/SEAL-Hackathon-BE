8fc762a0753a2c86c27e90ce11df67800f7fc2c3
Simple <vanchien0307206@gmail.com>
2026-06-11 13:07:54 +0700

    Fix AwardRepo

--- src/main/java/com/fpt/swp/sealhackathonbe/award/repository/AwardRepository.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/award/repository/AwardRepository.java
@@ -2,12 +2,14 @@ package com.fpt.swp.sealhackathonbe.award.repository;
 
 import com.fpt.swp.sealhackathonbe.award.entity.Award;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface AwardRepository extends JpaRepository<Award, UUID> {
-    List<Award> findByEvent_EventId(UUID eventId);
-
+public interface AwardRepository extends JpaRepository<Award, Long> {
+    Optional<Award> findById(UUID id);
+    List<Award> findByEventId(UUID eventId);
     List<Award> findByIsPublishedTrueOrderByAwardedAtDesc();
 }
