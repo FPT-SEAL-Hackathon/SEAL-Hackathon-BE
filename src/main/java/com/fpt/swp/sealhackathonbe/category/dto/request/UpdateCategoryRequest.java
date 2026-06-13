4b079d98953a969aae6811194bd9738f86fb6f40
thaovann09 <thaovann1405@gmail.com>
2026-06-13 13:33:42 +0700

    feat/categories: Add CRUD for Category

--- src/main/java/com/fpt/swp/sealhackathonbe/category/dto/request/UpdateCategoryRequest.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/category/dto/request/UpdateCategoryRequest.java
@@ -1,4 +1,17 @@
 package com.fpt.swp.sealhackathonbe.category.dto.request;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
 public class UpdateCategoryRequest {
+    private String categoryName;
+    private String description;
+    private Integer sortOrder;
+    private Boolean isActive;
 }
