fab59bfd5ffc9f5df3b7ae03b5af0377c51d819d
Simple <vanchien0307206@gmail.com>
2026-07-27 14:46:15 +0700

    Update Create Database.sql

--- database/Create Database.sql
+++ database/Create Database.sql
@@ -3099,7 +3099,43 @@ END;
 END;
 GO
 
+IF NOT EXISTS (
+    SELECT 1
+    FROM dbo.EventStatus
+    WHERE StatusID = '30000000-0000-0000-0000-000000000006'
+)
+BEGIN
+INSERT INTO dbo.EventStatus
+(
+    StatusID,
+    StatusName
+)
+VALUES
+    (
+        '30000000-0000-0000-0000-000000000006',
+        'Upcoming'
+    );
+END
+GO
 
+IF NOT EXISTS (
+    SELECT 1
+    FROM dbo.EventStatus
+    WHERE StatusID = '30000000-0000-0000-0000-000000000007'
+)
+BEGIN
+INSERT INTO dbo.EventStatus
+(
+    StatusID,
+    StatusName
+)
+VALUES
+    (
+        '30000000-0000-0000-0000-000000000007',
+        'Registration Closed'
+    );
+END
+GO
 -- Create Appeals table
 CREATE TABLE Appeals (
     AppealID UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
