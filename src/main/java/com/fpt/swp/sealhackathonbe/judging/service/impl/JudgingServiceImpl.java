eef6e76bc9cc7ccb5ae19fb3658aa0a9e5fb4d3d
Simple <vanchien0307206@gmail.com>
2026-07-29 14:33:31 +0700

    fix: add Transactional readOnly to services to prevent LazyInitializationException

--- src/main/java/com/fpt/swp/sealhackathonbe/judging/service/impl/JudgingServiceImpl.java
+++ src/main/java/com/fpt/swp/sealhackathonbe/judging/service/impl/JudgingServiceImpl.java
@@ -37,6 +37,7 @@ import com.fpt.swp.sealhackathonbe.team.repository.TeamMembersRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class JudgingServiceImpl implements JudgingService {
 
     private final JudgingRepository judgingRepository;
